a4a9f69ce477c1e19620cb6d89f48041ad2bc4ac
SamiiKeller <samiikeller@hotmail.com>
2018-11-25 14:45:21 -0600

    user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #devise_for :users
   resources :registers
   resources :events
   resources :reservations
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   resources :students
   resources :areas
   resources :places
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
